be228ccef3b7a89220ef3d3abc2f52750727db41
NguyenHuyy0905 <huythuongtin@gmail.com>
2023-05-20 14:27:52 +0700

    Upload bai tap AJS buoi 2

---
+++ 20230518 AJS Buoi_02 BT_01_Quiz/script.js
@@ -0,0 +1,227 @@
+var questions = [
+  {
+    index: 1,
+    questionText: "Địa phương nào từng được gọi là Thanh Hoa ngoại trấn?",
+    anwsers: ["Thanh Hóa", "Ninh Bình", "Bình Định"],
+    correctAnswer: "Ninh Bình",
+    indexCorrectAnswer: 1,
+    percent: ["32%", "62%", "06%"],
+    percentCorrect: "62%",
+    point: 1,
+    script:
+      "Vùng đất Ninh Bình ngày nay từng được gọi là Thanh Hoa ngoại trấn từ thời nhà Mạc. Năm 968, sau khi đánh dẹp và thu phục 12 sứ quân, Đinh Bộ Lĩnh lên ngôi hoàng đế, chọn Hoa Lư làm kinh đô, đặt tên nước là Đại Cồ Việt. Từ thời kỳ nhà Đinh cho tới nhà Nguyễn Tây Sơn, vùng đất này đã qua nhiều tên gọi: Trường Yên, Đại Hoàng, Thiên Quan rồi lại đổi thành Trường Yên. Từ thời nhà Mạc đến thời nhà Nguyễn, nơi đây được gọi là Thanh Hoa ngoại trấn. Năm Gia Long thứ 5, nơi đây đổi thành đạo Thanh Bình. Tới năm Minh Mạng thứ 3 (1822), đạo Thanh Bình được đổi thành đạo Ninh Bình, tên gọi Ninh Bình xuất hiện từ đó. Năm Minh Mệnh thứ 12 (1831), vua đổi trấn Ninh Bình thành tỉnh Ninh Bình.",
+    correctColor: "rgba(0, 128, 0, 0.6)",
+    inCorrectColor: "rgba(255, 0, 0, 0.6)",
+  },
+  {
+    index: 2,
+    questionText: "Tên Ninh Bình có nghĩa là gì?",
+    anwsers: [
+      "Vùng đất được dẹp yên, bình ổn",
+      "Vùng đất an toàn, bình yên",
+      "Vùng đất có mọi thứ",
+    ],
+    correctAnswer: "Vùng đất an toàn, bình yên",
+    indexCorrectAnswer: 1,
+    percent: ["34%", "54%", "12%"],
+    percentCorrect: "54%",
+    point: 1,
+    script:
+      'Trong tên gọi Ninh Bình, từ "Ninh" có nghĩa là an toàn, vững chãi, "Bình" có nghĩa là bình yên. Tên gọi Ninh Bình với hàm ý là vùng đất an toàn, vững chãi, bình yên. Tỉnh Ninh Bình có diện tích tự nhiên gần 1.400 km2. Nơi đây có biển, rừng núi, đồng bằng, giàu tiềm năng phát triển toàn diện và là một địa bàn có vị trí quan trọng về quân sự, tiến có thể đánh, lui có thế giữ.',
+    correctColor: "rgba(0, 128, 0, 0.6)",
+    inCorrectColor: "rgba(255, 0, 0, 0.6)",
+  },
+  {
+    index: 3,
+    questionText: "Hoa Lư là nơi khởi đầu của bao nhiêu triều đại?",
+    anwsers: [1, 3, 2],
+    correctAnswer: 3,
+    indexCorrectAnswer: 1,
+    percent: ["18%", "45%", "37%"],
+    percentCorrect: "45%",
+    point: 1,
+    script:
+      "Hoa Lư là nơi phát tích của ba vương triều Ðinh, tiền Lê và Lý. Ngày nay, dấu tích của cố đô Hoa Lư thuộc xã Trường Yên, huyện Hoa Lư, tỉnh Ninh Bình, cách Hà Nội gần 100 km về phía nam. Hoa Lư được chọn là kinh đô đầu tiên của nhà nước phong kiến tập quyền khi Đinh Bộ Lĩnh lên ngôi hoàng đế năm 968. Nơi đây có lợi thế quân sự với địa thế đồi núi trùng điệp, bao bọc xung quanh vành đai kinh đô như tấm bình phong vững chãi, cùng dòng sông Hoàng Long uốn khúc và cánh đồng Nho Quan, Gia Viễn với hào sâu. 29 năm triều tiền Lê (980-1009) sau đó và thời kỳ đầu nhà Lý (1009-1010) cũng gắn với mảnh đất Hoa Lư. Đến năm 1010, vua Lý Thái Tổ quyết định dời đô về hoàng thành Thăng Long – Hà Nội .",
+    correctColor: "rgba(0, 128, 0, 0.6)",
+    inCorrectColor: "rgba(255, 0, 0, 0.6)",
+  },
+  {
+    index: 4,
+    questionText: "Nam thiên đệ nhị động là biệt danh của động nào?",
+    anwsers: ["Địch Lộng", "Bích Động", "Hương Tích"],
+    correctAnswer: "Bích Động",
+    indexCorrectAnswer: 1,
+    percent: ["5%", "75%", "20%"],
+    percentCorrect: "75%",
+    point: 1,
+    script:
+      "Bích Động được biết đến với danh xưng Nam thiên đệ nhị động, tức là động đẹp thứ hai ở trời Nam. Bích Động nằm trong khu du lịch Tam Cốc-Bích Động, có diện tích tự nhiên 350 ha, nằm cách quốc lộ 1A khoảng 2 km, cách thành phố Ninh Bình 7 km. Để tham quan, du khách phải đi bằng thuyền, xuôi theo dòng sông Ngô Đồng thơ mộng. Các ngọn núi ở đây có hình thù đa dạng, nối tiếp nhau.",
+    correctColor: "rgba(0, 128, 0, 0.6)",
+    inCorrectColor: "rgba(255, 0, 0, 0.6)",
+  },
+  {
+    index: 5,
+    questionText: "Ai đặt biệt danh cho Bích Động là Nam thiên đệ nhị động?",
+    anwsers: [
+      "Chúa Trịnh Sâm",
+      "Vua Đinh Tiên Hoàng",
+      "Cha của đại thi hào Nguyễn Du",
+    ],
+    correctAnswer: "Cha của đại thi hào Nguyễn Du",
+    indexCorrectAnswer: 2,
+    percent: ["42%", "38%", "21%"],
+    percentCorrect: "21%",
+    point: 1,
+    script:
+      "Trong chuyến đi tuần cùng với chúa Trịnh Sâm vào năm Giáp Ngọ (1774), tể tướng Nguyễn Nghiễm, cha của đại thi hào Nguyễn Du, thấy động vừa đẹp đẽ vừa nguy nga liền gọi nơi đây là Nam thiên đệ nhị động, đứng sau động Hương Tích và trước động Địch Lộng. Cũng trong chuyến đi tuần này, chúa Trịnh Sâm viếng một ngôi chùa bên ngoài động, nhìn toàn cảnh núi non, sông nước, đồng ruộng, cây cối xanh tươi liền đổi tên nơi đây thành Bích Động, có nghĩa là động xanh. Dòng chữ này được chạm khắc ở vách đá thẳng đứng bên trên mái chùa.",
+    correctColor: "rgba(0, 128, 0, 0.6)",
+    inCorrectColor: "rgba(255, 0, 0, 0.6)",
+  },
+];
+
+var next_Button = document.getElementById("next-btn");
+var prev_Button = document.getElementById("prev-btn");
+var quiz_main_more_information = document.getElementById(
+  "quiz-main__more-information"
+);
+var quiz_main_question = document.getElementById("quiz-main__question");
+var quiz_main_answers = document.getElementById("quiz-main__answers");
+var display_information_content = document.getElementById(
+  "information__content"
+);
+var information = document.getElementById("information");
+var quiz_main = document.getElementById("quiz__main");
+var display = "";
+
+var index = 0;
+var chooseAnsValues = [];
+// component
+var component = (function () {
+  // loadQuestion
+  var loadQuestion = (question, index) => {
+    let chooseAnsValue = chooseAnsValues[index];
+    let text_1 = "";
+    let text_2 = "";
+    let text_3 = "";
+    text_1 += `
+    <span id="question-index">${question.index}</span>/<span id="question-total">${questions.length}</span
+    >.
+    <span id="question-content"
+      >${question.questionText}</span
+    >`;
+    text_2 += `
+    <ul>`;
+    question.anwsers.forEach((value, index) => {
+      text_2 +=
+        `<li id="li-answer-${index}">
+      <input type="radio" name="answer" id="${index}"` +
+        (chooseAnsValue !== undefined && chooseAnsValue == value
+          ? "checked"
+          : "") +
+        ` value="${value}" />
+      <span id="content-answer-${index}">${value}</span>
+      <span class="answer-percent" id="answer-percent-${index}" style="display: ${display}">${question.percent[index]}</span>
+    </li>`;
+    });
+
+    text_2 += `</ul>`;
+    text_3 += `<span id="percent-correct">${question.percentCorrect}</span> người chơi trả lời đúng câu
+    hỏi này`;
+    var answer_percent = document.querySelector(
+      "#quiz-main__answers li .answer-percent"
+    );
+    console.log(answer_percent);
+    display_information_content.innerText = question.script;
+    quiz_main_question.innerHTML = text_1;
+    quiz_main_answers.innerHTML = text_2;
+    quiz_main_more_information.innerHTML = text_3;
+    // quiz_main.style.display = "block";
+  };
+  // loadPage
+  var loadPage = function () {
+    // next_Button
+    next_Button.addEventListener("click", function () {
+      let currentQuestion = questions[index];
+      let chooseAnsValue;
+      currentQuestion.anwsers.forEach((value, index) => {
+        if (document.getElementById(index).checked) {
+          chooseAnsValue = document.getElementById(index).value;
+        }
+      });
+      chooseAnsValues[index] = chooseAnsValue;
+      index++;
+      prev_Button.style.display = "block";
+      if (parseInt(index) === parseInt(questions.length - 1))
+        next_Button.style.display = "none";
+      loadQuestion(questions[index], index);
+      information.style.display = "none";
+      quiz_main_more_information.style.display = "none";
+      more_information();
+    });
+    // prev_Button
+    prev_Button.addEventListener("click", function () {
+      let currentQuestion = questions[index];
+      let chooseAnsValue;
+      currentQuestion.anwsers.forEach((value, index) => {
+        if (document.getElementById(index).checked) {
+          chooseAnsValue = document.getElementById(index).value;
+        }
+      });
+      chooseAnsValues[index] = chooseAnsValue;
+      index--;
+      next_Button.style.display = "block";
+      if (parseInt(index) === 0) {
+        prev_Button.style.display = "none";
+        next_Button.style.display = "block";
+      }
+      loadQuestion(questions[index], index);
+      information.style.display = "none";
+      quiz_main_more_information.style.display = "none";
+      more_information();
+    });
+  };
+
+  var more_information = function () {
+    let currentQuestion = questions[index];
+    var check_test = document.querySelectorAll("input");
+    var check_percent = document.querySelectorAll(
+      "#quiz-main__answers li .answer-percent"
+    );
+    var check_colors = document.querySelectorAll(
+      "#quiz-main__answers li input"
+    );
+    console.log(check_colors[0]);
+    console.log(typeof check_percent);
+    check_test.forEach((value, index) => {
+      // console.log(check_test[index]);
+      check_test[index].addEventListener("click", () => {
+        quiz_main_more_information.style.display = "block";
+        information.style.display = "block";
+        check_percent.forEach((value, index) => {
+          check_percent[index].style.display = "inline-block";
+        });
+        check_colors.forEach((check_color, index) => {
+          check_color.parentElement.style.background =
+            currentQuestion.inCorrectColor;
+          check_colors[
+            currentQuestion.indexCorrectAnswer
+          ].parentElement.style.background = currentQuestion.correctColor;
+        });
+      });
+    });
+  };
+
+  // more_information();
+
+  // return
+  return {
+    init: function () {
+      loadQuestion(questions[index], index);
+      loadPage();
+      more_information();
+    },
+  };
+})();
+document.addEventListener("DOMContentLoaded", function () {
+  component.init();
+});
